1cf34a9896a9b6347d10b1df2805c3ee8b44d0ef
Muslehud77 <fardinmohit@gmail.com>
2024-05-01 08:49:08 +0800

    learning the basic data types of typescript

---
+++ src/BasicDataType.ts
@@ -0,0 +1,35 @@
+// Basic Data type
+
+
+// string
+
+let firstName : string = "musleh"
+
+//number
+
+let roll : number = 123
+
+// boolean
+
+let isAdmin : boolean = true
+
+// undefined
+
+let x : undefined = undefined
+
+// null
+
+let y : null = null
+
+
+//Array
+
+let friends : string[] = ['rachel','monica']
+
+let marks : number[] = [66,99]
+
+// tuple --> array --> order of value type
+
+let coordinates : [number,number] = [1,5]
+
+let ageName : [number,string] = [25,'musleh']
