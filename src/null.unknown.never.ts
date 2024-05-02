8a726ede635c6a4abe165b531cc33215f86b917f
Muslehud77 <fardinmohit@gmail.com>
2024-05-02 22:07:47 +0800

    learned about typeScript null,unknown,never

---
+++ src/null.unknown.never.ts
@@ -0,0 +1,39 @@
+{
+// nullable types/ unknown types
+
+const searchName = (value:string|null)=>{
+    if(value){
+        console.log("searching...");
+    }else{
+        console.log("there is nothing to search");
+    }
+}
+
+searchName(null)
+
+// unknown
+
+const getSpeedInMeterPerSecond= (value:unknown)=>{
+    if(typeof value === 'number'){
+        const convertedSpeed = (value*1000)/3600
+        console.log(`The speed is ${convertedSpeed} ms^-1`);
+    }
+    else if(typeof value === 'string'){
+        const speed = parseInt(value.split(' ')[0])
+        const convertedSpeed = (speed*1000)/3600
+        console.log(`The speed is ${convertedSpeed} ms^-1`);
+    }else{
+        console.log("wrong input");
+    }
+}
+getSpeedInMeterPerSecond('1000 kmh')
+
+// never
+
+function throwError(msg:string):never{
+    throw new Error(msg)
+}
+
+throwError('error')
+
+}
